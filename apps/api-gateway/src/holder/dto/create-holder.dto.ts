17b1fa1a62e0a8e5aa12fd828829f5ee367b6246
amrullh <168280074+amrullh@users.noreply.github.com>
2025-07-30 17:02:00 +0800

    hmm tambah tambah index db

--- apps/api-gateway/src/holder/dto/create-holder.dto.ts
+++ apps/api-gateway/src/holder/dto/create-holder.dto.ts
@@ -1,6 +1,25 @@
+// apps/api-gateway/src/holder/dto/create-holder.dto.ts
+import { IsString, IsEmail, IsNotEmpty } from 'class-validator';
 
 export class CreateHolderDto {
-    name: string;
-    nik: string;
-    dob: string;
+    @IsString()
+    @IsNotEmpty()
+    uid: string; // Firebase UID
+
+    @IsEmail()
+    @IsNotEmpty()
+    email: string;
+
+    @IsString()
+    @IsNotEmpty()
+    fullName: string;
+
+    // Anda akan menambahkan ini nanti:
+    // @IsString()
+    // @IsNotEmpty()
+    // walletAddress?: string;
+
+    // @IsString() // ZKP proof adalah string JSON atau sejenisnya
+    // @IsNotEmpty()
+    // zkProof?: string;
 }
